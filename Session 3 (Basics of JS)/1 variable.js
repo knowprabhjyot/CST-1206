d21cfc3ff13caef8d91ff83e332659eeaa8c0429
Prabhjyot Gambhir <knowprabhjyot@gmail.com>
2022-09-20 10:44:42 -0700

    variables updated

--- Session 3 (Basics of JS)/1 variable.js
+++ Session 3 (Basics of JS)/1 variable.js
@@ -2,31 +2,31 @@
 
 // // Var
 
-// function sum() {
-//     var a;
-//     var b;
-//     // var b; // Declaration
-//     a = 10;
+function sum() {
+    var a;
+    var b;
+    // var b; // Declaration
+    a = 10;
     
-//     console.log(c); // Referrential error
-//     let  c = 10;
+    console.log(c); // Referrential error
+    let  c = 10;
 
-//     console.log(b, "Value of b");
+    console.log(b, "Value of b");
 
-//     if (true) {
-//         b = 11; // Assignment/Call
-//         let c = 10; // died within the block
-//         const d = 20; // died within the block
-//     }
+    if (true) {
+        b = 11; // Assignment/Call
+        let c = 10; // died within the block
+        const d = 20; // died within the block
+    }
 
-//     // console.log(a); // 10
-//     // console.log(b); // 11
-//     // console.log(c); // Error --> Referential Eror
-//     // console.log(d); // Error  Referential Error
-//     // c & d are in the temporal dead zone
-// }
+    // console.log(a); // 10
+    // console.log(b); // 11
+    // console.log(c); // Error --> Referential Eror
+    // console.log(d); // Error  Referential Error
+    // c & d are in the temporal dead zone
+}
 
-// sum();
+sum();
 
 
 function random() {
