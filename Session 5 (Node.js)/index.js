3f3a0487816dfd33c3e102488cdc5a728064ec77
Prabhjyot Gambhir <knowprabhjyot@gmail.com>
2022-10-04 10:45:02 -0700

    added gitignore and removed node_modules

--- Session 5 (Node.js)/index.js
+++ Session 5 (Node.js)/index.js
@@ -4,14 +4,25 @@ const PORT = 4000;
 
 // Create a server 
 const server = http.createServer((request, response) => {
+    response.setHeader('Content-type', "text/html");
 
     if (request.url === "/") {
         // Setting headers to text and html so the browser knows what
         // kind of response i will recieve
-        response.setHeader('Content-type', "text/html");
 
         // We are sending a response to the client/browser using response.write
         response.write("<h1>Hello Thankyou for contacting me! </h1>")
+
+        // Ending the response
+        response.end();
+    }
+
+    if (request.url === "/user") {
+        // Setting headers to text and html so the browser knows what
+        // kind of response i will recieve
+
+        // We are sending a response to the client/browser using response.write
+        response.write("<h1>Hello User! </h1>")
 
         // Ending the response
         response.end();
