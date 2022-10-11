fe65d440ce4eb5499a25bbe498a54dd674ba5fe9
Prabhjyot Gambhir <knowprabhjyot@gmail.com>
2022-10-11 12:30:22 -0700

    express get post basics

--- Session 6/Express Server/index.js
+++ Session 6/Express Server/index.js
@@ -2,13 +2,31 @@ const express = require('express');
 
 const app = express();
 const PORT = 4000;
+const student = [];
+
+// Telling express that the data i am getting is of type JSON
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send("Welcome!");
+    res.send("Welcome to Student APIs!");
 })
 
-app.get('/user', (req, res) => {
-    res.send("Hello User!");
+// We are trying to get students data from the server
+app.get('/students', (req, res) => {
+    // For get requests we give 200 success response
+    // const data = res.status(200);
+    // return data.json(student);
+
+    return res.status(200).json(student);
+})
+
+// We are trying to post students data to the server
+app.post('/students', (req, res) => {
+    const studentData = req.body;
+    student.push(studentData);
+
+    // 201 is used when you are trying to create a resource and send sucess response
+    return res.status(201).json(student);
 })
 
 app.listen(PORT, () => {
